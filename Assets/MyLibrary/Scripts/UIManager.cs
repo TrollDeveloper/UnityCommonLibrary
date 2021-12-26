bc50c0b1d5679d7fa84081b47928b93a8d95e394
Kim-PC\Kim <trolldeveloper@naver.com>
2021-12-26 22:09:57 +0900

    UI Manager 관련 Base 추가

---
+++ Assets/MyLibrary/Scripts/UIManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.Serialization;
+using Sirenix.OdinInspector;
+using CodeControl;
+using EasyMobile.Demo;
+using JetBrains.Annotations;
+using UI.Dialog;
+
+namespace UI
+{
+    public class UIManager : MonoBehaviourSingleton<UIManager>
+    {
+        private Dictionary<Type, DialogBase> dialogMap = new Dictionary<Type, DialogBase>();
+
+        protected void Awake()
+        {
+            base.Awake();
+            var dialogs = transform.GetComponentsInChildren<DialogBase>(true);
+            for (int i = 0; i < dialogs.Length; i++)
+            {
+                Type type = dialogs[i].GetType();
+                
+                if (dialogMap.ContainsKey(type) == false)
+                {
+                    dialogMap.Add(type, dialogs[i]);
+                }
+            }
+
+            ResetDialog();
+        }
+
+        public void ResetDialog()
+        {
+            foreach (KeyValuePair<Type, DialogBase> keyValuePair in dialogMap)
+            {
+                keyValuePair.Value.gameObject.SetActive(false);
+            }
+        }
+
+        public void RequestDialogEnter<T>() where T : DialogBase
+        {
+            SetActiveUI<T>(true);
+        }
+
+        public void RequestDialogExit<T>() where T : DialogBase
+        {
+            SetActiveUI<T>(false);
+        }
+
+        void SetActiveUI<T>(bool isActive) where T : DialogBase
+        {
+            Type type = typeof(T);
+            if (dialogMap.ContainsKey(type))
+            {
+                var dialog = dialogMap[type];
+                dialog.gameObject.SetActive(isActive);
+            }
+        }
+    }
+}
