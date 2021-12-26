87f59a3b04d3165fa3a8649c42c35452b84df231
Kim-PC\Kim <trolldeveloper@naver.com>
2021-12-26 22:14:15 +0900

    서브모듈화 위해 폴더구조 변경

--- Scripts/DebugLog.cs
+++ Scripts/DebugLog.cs
@@ -10,12 +10,14 @@ public class DebugLog
         Debug.Log(message);
 #endif
     }
+
     public static void LogWarning(object message)
     {
 #if UNITY_EDITOR || Debug
         Debug.LogWarning(message);
 #endif
     }
+
     public static void LogError(object message)
     {
 #if UNITY_EDITOR || Debug
